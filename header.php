864ce9f603d575c86b8cb8c7345d37bdad0015eb
Amani Konduru <akonduru2@student.gsu.edu>
2017-04-28 01:52:25 -0400

    change in logo and cart
    update

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 <div id="header">
-		<table width="100%" border="0" cellspacing="0" cellpadding="12"> 
+		<table width="100%" border="0" cellspacing="0" cellpadding="12">
 		<tr>
-			<td width="32%"><a href="index.php"><img src="" alt="logo" width="250" height="36" border="0">
+			<td width="32%"><a href="index.php"><img src="logo.jpg" alt="logo" width="250%" height="36%" border="0">
 			<td width="68%">&nbsp;</td>
-			<td width="68%" align="right"><a href="cart.php">Your Cart</a></td>
-   
+			<td width="68%" align="right"><a href="cart.php"><img src="cart.png" alt="Your Cart" width="250%" height="36%" border="0">
+
 		</tr>
 		<tr>
 		<ul>
@@ -18,6 +18,5 @@
 			</ul>
 		</tr>
 		</table>
-		
-	</div>
 
+	</div>
