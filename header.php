acfb25b311c4012c85993ba5fcc4fa1d05b36897
Amani Konduru <akonduru2@student.gsu.edu>
2017-04-28 01:55:14 -0400

    logo and cart change in size

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <div id="header">
 		<table width="100%" border="0" cellspacing="0" cellpadding="12">
 		<tr>
-			<td width="32%"><a href="index.php"><img src="logo.jpg" alt="logo" width="250%" height="36%" border="0">
+			<td width="32%"><a href="index.php"><img src="logo.jpg" alt="logo" width="30%" height="36%" border="0">
 			<td width="68%">&nbsp;</td>
-			<td width="68%" align="right"><a href="cart.php"><img src="cart.png" alt="Your Cart" width="250%" height="36%" border="0">
+			<td width="68%" align="right"><a href="cart.php"><img src="cart.png" alt="Your Cart" width="30%" height="36%" border="0">
 
 		</tr>
 		<tr>
